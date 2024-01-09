53fea4dff2987ac4c1f593ae8d9080f191ea4639
Disha Sarvaiya <79967854+venellope04@users.noreply.github.com>
2024-01-10 01:45:01 +0530

    i

--- screener1.py
+++ screener1.py
@@ -5,3 +5,17 @@ import datetime as dt
 from tkinter import Tk 
 from Tkinter.filedialog import askopenfilename
 
+yf.pdr_override()
+start =dt.datetime(2018,12,1)
+now=dt.datetime.now()
+
+root= Tk()
+ftypes=[(".xlsm",".xlsx","xls")]
+ttl="Title"
+dir1='c:\\'
+filePath=
+
+stocklist=pd.read_excel(filePath)
+stocklist=stocklist.head()
+
+exportList=pd.DataFrame(colums=['stock',"RS_rating","50 Day MA","200 day Ma","52 week high"])
