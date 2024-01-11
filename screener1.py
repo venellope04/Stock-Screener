989e782401b2b1c4f2846e83f4eb3362ab518e5f
Disha Sarvaiya <79967854+venellope04@users.noreply.github.com>
2024-01-12 01:54:01 +0530

    changes

--- screener1.py
+++ screener1.py
@@ -46,7 +46,63 @@ for i in stocklist.index:
 
             except Exception:
                 moving_average_200_20=0
+
+#Condition 1: Current Price > 150 SMA and > 200 SMA
+		if(currentClose>moving_average_150>moving_average_200):
+			cond_1=True
+		else:
+			cond_1=False
+		#Condition 2: 150 SMA and > 200 SMA
+		if(moving_average_150>moving_average_200):
+			cond_2=True
+		else:
+			cond_2=False
+		#Condition 3: 200 SMA trending up for at least 1 month (ideally 4-5 months)
+		if(moving_average_200>moving_average_200_20):
+			cond_3=True
+		else:
+			cond_3=False
+		#Condition 4: 50 SMA> 150 SMA and 50 SMA> 200 SMA
+		if(moving_average_50>moving_average_150>moving_average_200):
+			#print("Condition 4 met")
+			cond_4=True
+		else:
+			#print("Condition 4 not met")
+			cond_4=False
+		#Condition 5: Current Price > 50 SMA
+		if(currentClose>moving_average_50):
+			cond_5=True
+		else:
+			cond_5=False
+		#Condition 6: Current Price is at least 30% above 52 week low (Many of the best are up 100-300% before coming out of consolidation)
+		if(currentClose>=(1.3*low_of_52week)):
+			cond_6=True
+		else:
+			cond_6=False
+		#Condition 7: Current Price is within 25% of 52 week high
+		if(currentClose>=(.75*high_of_52week)):
+			cond_7=True
+		else:
+			cond_7=False
+		#Condition 8: IBD RS rating >70 and the higher the better
+		if(RS_Rating>70):
+			cond_8=True
+		else:
+			cond_8=False
+		
+		if(cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7 and cond_8):
+			exportList = exportList.append({'Stock': stock, "RS_Rating": RS_Rating, "50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
+	except Exception:
+		print("No data on "+stock)
+
+print(exportList)
+
+newFile=os.path.dirname(filePath)+"/ScreenOutput.xlsx"
+
+writer= ExcelWriter(newFile)
+exportList.to_excel(writer,"Sheet1")
+writer.save()           
                 
 
-    except Exception:
+    
 
